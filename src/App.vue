53ab08cb1262c7ec6f8d8b7c1989f705e57348d1
limu <1259567994@qq.com>
2022-07-21 20:58:13 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MyHeader title="我爱一条柴"></MyHeader>
+    <div style="margin-top: 45px">
+      <component :is="comName"></component>
+    </div>
+    <MyTabBar :list="tabList" @change="changeFn"></MyTabBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from "./components/MyHeader.vue";
+import MyTabBar from "./components/MyTabBar.vue";
+import MyGoodsList from "./views/MyGoodList.vue";
+import MyGoodsSearch from "./views/MySearch.vue";
+import MyUserInfo from "./views/MyInfo.vue";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      comName: "MyGoodsList",
+      tabList: [
+        {
+          iconText: "icon-shangpinliebiao",
+          text: "商品列表",
+          componentName: "MyGoodsList",
+        },
+        {
+          iconText: "icon-sousuo",
+          text: "商品搜索",
+          componentName: "MyGoodsSearch",
+        },
+        {
+          iconText: "icon-user",
+          text: "我的信息",
+          componentName: "MyUserInfo",
+        },
+      ],
+    };
+  },
+  methods: {
+    changeFn(val) {
+      this.comName = val;
+    },
+  },
+  
   components: {
-    HelloWorld
-  }
-}
+    MyHeader,
+    MyTabBar,
+    MyGoodsList,
+    MyGoodsSearch,
+    MyUserInfo,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import "bootstrap/dist/css/bootstrap.css"
+
+import "./assets/fonts/iconfont.css"
+import axios from "axios"
+axios.defaults.baseURL = "https://www.escook.cn/"
+Vue.prototype.$axios = axios
+Vue.directive("focus", {
+  inserted(el) {
+    el.focus();
+  },
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
